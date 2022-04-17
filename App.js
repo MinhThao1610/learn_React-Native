2fb30fd5099a3ffe893470c6c313b3ca246fbf70
MinhThao1610 <minhthaoo1610@gmail.com>
2022-04-17 16:34:05 +0700

    add

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {createStackNavigator} from 'react-navigation'
+import MainRouteComponent from './screens/Main.js'
+import MovieRouteComponent from './screens/Movies.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const AppNavigator = createStackNavigator({
+  "MainRoute": MainRouteComponent,
+  "MovieRoute": MovieRouteComponent,
+})
+
+export default class App extends React.Component {
+  render() {
+    return <AppNavigator />
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
