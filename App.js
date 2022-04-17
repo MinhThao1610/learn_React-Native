77b0d26a1a60fe820edc70f84c7f04cdf39805b8
MinhThao1610 <minhthaoo1610@gmail.com>
2022-04-17 16:31:28 +0700

    add

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ContextProvider } from './context';
+import {
+  RootView,
+  Title,
+  DisplayTime,
+  SecondaryControls,
+  PrimaryControls
+} from './components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ContextProvider>
+      <RootView>
+        <Title />
+        <DisplayTime />
+        <SecondaryControls />
+        <PrimaryControls />
+      </RootView>
+    </ContextProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
